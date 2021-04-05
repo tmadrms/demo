dbbd67667006646db2b3659a6f525ae7911ba616
tmadrms <andrewcwright@gmail.com>
2021-04-05 12:30:40 -0700

    Add plugin to auto-purge cache

---
+++ plugins/index.js
@@ -0,0 +1,65 @@
+const fetch = require('node-fetch');
+
+const {
+  env: { FASTLY_API_TOKEN, FASTLY_SERVICE_ID},
+} = require('process');
+
+let authMethod = 'na';
+switch( true ) {
+  case typeof FASTLY_API_TOKEN !== 'undefined' && typeof FASTLY_SERVICE_ID !== 'undefined':
+    authMethod = 'TOKEN';
+    break;
+}
+
+module.exports = {
+  onPostBuild({
+    utils: {
+      build: { failBuild },
+    },
+  }) {
+    if( authMethod !== 'TOKEN' && authMethod !== 'KEY' ) {
+      return failBuild(
+          "'" + authMethod + "' is not a valid Authentication Method.  Please report this issue to the developer."
+      );
+    }
+    else {
+      console.log('FASTLY ' + authMethod + ' Authentication method detected.');
+    }
+  },
+  async onSuccess({
+                utils: {
+                  build: { failPlugin },
+                },
+              }) {
+    console.log('Preparing to trigger Fastly cache purge');
+    let baseUrl = `https://api.fastly.com/service/${FASTLY_SERVICE_ID}/purge_all`;
+    let headers;
+    switch( authMethod ) {
+      case 'TOKEN':
+        headers = {
+          'Fastly-Key': FASTLY_API_TOKEN,
+          'Content-Type': 'application/json'
+        };
+        break;
+
+    }
+    let body = { purge_everything: true };
+
+    try {
+      const { status, statusText } = await fetch(baseUrl, {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify(body),
+      });
+
+      if (status != 200) {
+        return failPlugin(
+            "Fastly cache couldn't be purged. Status: " + status + " " + statusText
+        );
+      }
+      console.log('Fastly cache purged successfully!');
+    } catch (error) {
+      return failPlugin('Fastly cache purge failed', { error });
+    }
+  },
+};
