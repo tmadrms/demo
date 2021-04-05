5423d7fcb0e32c4d2a6cb4f51ec656b6630566a9
tmadrms <andrewcwright@gmail.com>
2021-04-05 13:22:57 -0700

    fixed method

--- plugins/index.js
+++ plugins/index.js
@@ -49,7 +49,7 @@ module.exports = {
 
     try {
       const { status, statusText } = await fetch(baseUrl, {
-        method: 'PURGE',
+        method: 'POST',
         headers: headers,
         body: JSON.stringify(body),
       });
